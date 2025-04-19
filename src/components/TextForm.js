d714b7b0e72d84d848ee7cad6387e3f6e0731a9f
piysinha <piyushdinkarsinha95@gmail.com>
2025-04-19 14:20:51 +0530

    Disable Key Bug Fix
    Disabling the key when there are no text available in the box.

--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -34,6 +34,7 @@ export default function TextForm(props) {
         let text = document.getElementById("myBox");
         text.select();
         navigator.clipboard.writeText(text.value);
+        document.getSelection().removeAllRanges();
         props.showAlert("Copied to clipboard!","success");
     }
 
@@ -51,18 +52,18 @@ export default function TextForm(props) {
                 <div className="mb-3">
                     <textarea className="form-control" value={text} onChange={handleOnChange} style={{backgroundColor:props.mode==='dark'?'#293d61':'white', color:props.mode==='dark'?'white':'#020c37'}} id="myBox" rows="8"></textarea>
                 </div>
-                <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
-                <button className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
-                <button className="btn btn-primary mx-2 my-1" onClick={handleClearText}>Clear Text</button>
-                <button className="btn btn-primary mx-2 my-1" onClick={handleCopyText}>Copy Text</button>
-                <button className="btn btn-primary mx-2 my-1" onClick={handleExtraSpace}>Remove Extra Space</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleLoClick}>Convert to Lowercase</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleClearText}>Clear Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleCopyText}>Copy Text</button>
+                <button disabled={text.length===0} className="btn btn-primary mx-2 my-1" onClick={handleExtraSpace}>Remove Extra Space</button>
             </div>
             <div className='container my-3' style={{color:props.mode==='dark'?'white':'#020c37'}}>
                 <h3>Your text Summary</h3>
                 <p>{text.split(" ").filter((element)=>{return element.length!==0}).length} words and having {text.length} characters</p>
                 <p> {0.008 * text.split(" ").filter((e)=>{return e.length!==0}).length} Minutes Read</p>
                 <h2>Preview</h2>
-                <p>{text.length>0?text:"Enter your text in the above box to preview it here"}</p>
+                <p>{text.length>0?text:"Nothing to Preview!"}</p>
             </div>
         </>
     )
